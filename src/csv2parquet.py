2752c6eb33cd11fdd3780118aee74c18a2012708
Chen Huang <toocold@foxmail.com>
2024-01-22 16:13:06 +0100

    [init] melt csv and convert to parquet

---
+++ src/csv2parquet.py
@@ -0,0 +1,26 @@
+#%% Imports
+import polars as pl
+
+#%% Read CSV
+def melt_csv2parquet(scale):
+    path_dict = {
+        'r10': '../data/data_raw/AR6_Scenarios_Database_R10_regions_v1.1.csv',
+        'r5': '../data/data_raw/AR6_Scenarios_Database_R5_regions_v1.1.csv',
+        'r6': '../data/data_raw/AR6_Scenarios_Database_R6_regions_v1.1.csv',
+        'world': '../data/data_raw/AR6_Scenarios_Database_World_v1.1.csv'
+    }
+    df = pl.scan_csv(path_dict[scale])
+    df = (
+        df.melt(id_vars=['Model', 'Scenario', 'Region', 'Variable', 'Unit'], variable_name='year', value_name='value')
+        .rename({'Model': 'model', 'Scenario': 'scenario', 'Region': 'region', 'Variable': 'variable', 'Unit': 'unit'})
+        .drop_nulls(subset=['value'])
+    )
+    df.sink_parquet('../data/data_clean/' + scale + '.parquet')
+    print(scale + ' Done!')
+
+#%% Call function
+if __name__ == '__main__':
+    melt_csv2parquet('r10')
+    melt_csv2parquet('r5')
+    melt_csv2parquet('r6')
+    melt_csv2parquet('world')
