5f0d0da3189238941d73a7563daa81b80ff0b056
Chen Huang <toocold@foxmail.com>
2024-02-08 14:50:44 +0100

    [update] add category column

--- src/csv2parquet.py
+++ src/csv2parquet.py
@@ -10,11 +10,21 @@ def melt_csv2parquet(scale):
         'world': '../data/data_raw/AR6_Scenarios_Database_World_v1.1.csv'
     }
     df = pl.scan_csv(path_dict[scale])
+    category = pl.scan_csv('../data/data_raw/category.csv')
     df = (
         df.melt(id_vars=['Model', 'Scenario', 'Region', 'Variable', 'Unit'], variable_name='year', value_name='value')
         .rename({'Model': 'model', 'Scenario': 'scenario', 'Region': 'region', 'Variable': 'variable', 'Unit': 'unit'})
         .drop_nulls(subset=['value'])
+        .cast({'year': pl.Int32, 'value': pl.Float64})
     )
+    # join category
+    df = (
+        df.join(category, on=['model', 'scenario'], how='left')
+        .select(
+            pl.col('category','model','scenario','region','variable','unit','year','value')
+        )
+    )
+
     df.sink_parquet('../data/data_clean/' + scale + '.parquet')
     print(scale + ' Done!')
 
