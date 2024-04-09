ebb04111a1e992fef02d72bb26a5c02b95c87c2f
Chen Huang <toocold@foxmail.com>
2024-04-09 10:27:46 +0200

    [update] replace Wind with Onshore for specific models and scenarios
    These models and scenarios lack Onshore and Offshore data but contain Wind data.

--- src/r10_cap_renew.py
+++ src/r10_cap_renew.py
@@ -1,14 +1,27 @@
 import polars as pl 
 
-# Reading
+# Reading and Filtering
 cap_var = ['Capacity|Electricity|Solar|PV', 'Capacity|Electricity|Solar|CSP', 'Capacity|Electricity|Wind|Onshore', 'Capacity|Electricity|Wind|Offshore', 'Capacity|Electricity|Wind']
 
-# Filtering
+ms_wind = pl.read_csv('../data/data_import/wind_ms_check.csv')
+
 cap = (
     pl.scan_parquet('../data/data_clean/r10.parquet')
     .filter(pl.col('category').is_in(['C1','C2','C3','C4']))
     .filter(pl.col('variable').is_in(cap_var))
     .select(pl.exclude('category'))
+    .with_columns(
+        pl.when(
+            (pl.col('model').is_in(ms_wind['model'])) & 
+            (pl.col('scenario').is_in(ms_wind['scenario'])) &
+            (pl.col('variable') == 'Capacity|Electricity|Wind')
+            )
+        .then(
+            pl.lit('Capacity|Electricity|Wind|Onshore')
+            )
+        .otherwise(pl.col('variable'))
+        .alias('variable')
+    )
 )
 
 # Exporting
