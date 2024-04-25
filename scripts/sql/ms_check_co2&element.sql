957cf8606843b213cf7a8ed8c12f20a9ad4b155c
Chen Huang <toocold@foxmail.com>
2024-04-25 18:13:57 +0200

    [feat&fix] add ele and trp emissions; rename abatement column
    original cum_abatement is just the abatement compared to baseyear 2020; the cumsum's cumsum is the final cum_abatement;

--- scripts/sql/ms_check_co2&element.sql
+++ scripts/sql/ms_check_co2&element.sql
@@ -1,9 +1,23 @@
 @set r10_co2 = '/Volumes/T7 White/Dataset/AR6/data/data_task/r10_emissions_co2.parquet'
+@set r10_co2_ele = '/Volumes/T7 White/Dataset/AR6/data/data_task/r10_emissions_co2_ele.parquet'
+@set r10_co2_trp = '/Volumes/T7 White/Dataset/AR6/data/data_task/r10_emissions_co2_trp.parquet'
 @set element_dem = '/Users/chen/Code/critical_minerals/data/data_output/element_demand.parquet'
 
 
 create or replace view ms_co2 as
 select distinct model, scenario from read_parquet(${r10_co2});
+
+create or replace view ms_co2_ele as
+select distinct model, scenario 
+from read_parquet(${r10_co2_ele})
+order by model;
+
+
+create or replace view ms_co2_trp as
+select distinct model, scenario 
+from read_parquet(${r10_co2_trp})
+order by model;
+
 
 create or replace view ms_element as
 SELECT model, scenario FROM read_parquet(${element_dem})
@@ -11,4 +25,10 @@ SELECT model, scenario FROM read_parquet(${element_dem})
 		GROUP BY model, scenario;
 
 select count(*) from ms_co2 mc
+inner join ms_element me using(model, scenario);
+
+select count(*) from ms_co2_ele mel
+inner join ms_element me using(model, scenario);
+
+select count(*) from ms_co2_trp mtr
 inner join ms_element me using(model, scenario);
--- src/r10_emissions_co2.py
+++ src/r10_emissions_co2.py
@@ -34,6 +34,8 @@ def fit_spline(series, years, parse=False):
 
 # Reading and Filtering
 co2_var = ['Emissions|CO2']
+co2_ele_var = ['Emissions|CO2|Energy|Supply|Electricity']
+co2_trp_var = ['Emissions|CO2|Energy|Demand|Transportation']
 
 co2 = (
     pl.scan_parquet('../data/data_clean/r10.parquet')
@@ -47,7 +49,7 @@ co2 = (
 
 co2_interp = []
 for pairs, df in co2.group_by(['model','scenario','region']):
-    print('Interpolating for:', pairs)
+    print('Interpolating for co2:', pairs)
     co2_interp.append(
         pl.DataFrame(
             {
@@ -59,11 +61,73 @@ for pairs, df in co2.group_by(['model','scenario','region']):
                 'value': fit_spline(df['value'], df['year']),
             }
         )
-        .with_columns(abatement=pl.col('value').diff().neg())
-        .with_columns(cum_abatement=pl.col('abatement').cumsum())
+        .with_columns(abatement_yearly=pl.col('value').diff().neg())
+        .with_columns(abatement_baseyear=pl.col('abatement_yearly').cumsum())
     )
-
 co2 = pl.concat(co2_interp)
+
+co2_ele = (
+    pl.scan_parquet('../data/data_clean/r10.parquet')
+    .filter(pl.col('category').is_in(['C1','C2','C3','C4']))
+    .filter(pl.col('variable').is_in(co2_ele_var))
+    .select(pl.exclude('category'))
+    .filter(pl.col('year').is_between(2020, 2060))
+    .sort('model','scenario','region','year')
+    .collect()
+)
+
+co2_ele_interp = []
+for pairs, df in co2_ele.group_by(['model','scenario','region']):
+    print('Interpolating for co2_ele:', pairs)
+    co2_ele_interp.append(
+        pl.DataFrame(
+            {
+                'model': pairs[0],
+                'scenario': pairs[1],
+                'region': pairs[2],
+                'unit': 'Mt CO2/yr',
+                'year': np.arange(2020, 2061),
+                'value': fit_spline(df['value'], df['year']),
+            }
+        )
+        .with_columns(abatement_yearly=pl.col('value').diff().neg())
+        .with_columns(abatement_baseyear=pl.col('abatement_yearly').cumsum())
+    )
+co2_ele = pl.concat(co2_ele_interp)
+
+co2_trp = (
+    pl.scan_parquet('../data/data_clean/r10.parquet')
+    .filter(pl.col('category').is_in(['C1','C2','C3','C4']))
+    .filter(pl.col('variable').is_in(co2_trp_var))
+    .select(pl.exclude('category'))
+    .filter(pl.col('year').is_between(2020, 2060))
+    .sort('model','scenario','region','year')
+    .collect()
+)
+
+co2_trp_interp = []
+for pairs, df in co2_trp.group_by(['model','scenario','region']):
+    print('Interpolating for co2_trp:', pairs)
+    co2_trp_interp.append(
+        pl.DataFrame(
+            {
+                'model': pairs[0],
+                'scenario': pairs[1],
+                'region': pairs[2],
+                'unit': 'Mt CO2/yr',
+                'year': np.arange(2020, 2061),
+                'value': fit_spline(df['value'], df['year']),
+            }
+        )
+        .with_columns(abatement_yearly=pl.col('value').diff().neg())
+        .with_columns(abatement_baseyear=pl.col('abatement_yearly').cumsum())
+    )
+co2_trp = pl.concat(co2_trp_interp)
 
 # Exporting
 co2.write_parquet('../data/data_task/r10_emissions_co2.parquet')
+print('co2 done')
+co2_ele.write_parquet('../data/data_task/r10_emissions_co2_ele.parquet')
+print('co2_ele done')
+co2_trp.write_parquet('../data/data_task/r10_emissions_co2_trp.parquet')
+print('co2_trp done')
