488a4cc9f19814d6c4971819cd3601a6f46d1103
Chen Huang <toocold@foxmail.com>
2024-06-22 22:30:27 +0200

    [sql] update check for transportation

--- scripts/sql/ms_check_trp_ele.sql
+++ scripts/sql/ms_check_trp_ele.sql
@@ -6,7 +6,7 @@ LOAD postgres;
 ATTACH 'dbname=critical_minerals user=postgres host=127.0.0.1' AS db1 (TYPE POSTGRES, READ_ONLY);
 
 
-CREATE OR REPLACE VIEW trp as (
+CREATE OR REPLACE VIEW trp_ele as (
 	select * from read_parquet(${ar6_task} || 'trp_ele_future.parquet')
 );
 
@@ -14,7 +14,56 @@ CREATE OR REPLACE VIEW trp as (
 select model, scenario from db1.transform.element_demand_region edr
 group by model, scenario
 except
-select model, scenario from trp 
+select model, scenario from trp_ele
 group by model, scenario;
+
+CREATE or replace temp table trp_check_rdft as (
+	select model, scenario, variable from read_parquet(${ar6_clean} || 'r10.parquet')
+	where 
+		variable = 'Final Energy|Transportation|Road|Freight'
+		and category in ('C1', 'C2', 'C3', 'C4')
+	group by model, scenario, variable
+);
+
+CREATE or replace temp table trp_check_ftele as (
+	select model, scenario, variable from read_parquet(${ar6_clean} || 'r10.parquet')
+	where 
+		variable = 'Final Energy|Transportation|Freight|Electricity'
+		and category in ('C1', 'C2', 'C3', 'C4')
+	group by model, scenario, variable
+);
+
+
+
+CREATE or replace temp table trp_check_rdps as (
+	select model, scenario, variable from read_parquet(${ar6_clean} || 'r10.parquet')
+	where 
+		variable = 'Final Energy|Transportation|Road|Passenger'
+		and category in ('C1', 'C2', 'C3', 'C4')
+	group by model, scenario, variable
+);
+
+CREATE or replace temp table trp_check_psele as (
+	select model, scenario, variable from read_parquet(${ar6_clean} || 'r10.parquet')
+	where 
+		variable = 'Final Energy|Transportation|Passenger|Electricity'
+		and category in ('C1', 'C2', 'C3', 'C4')
+	group by model, scenario, variable
+);
+
+
+select model, scenario from trp_check_rdft;  -- 32
+select model, scenario from trp_check_rdps;  -- 32
+
+select model, scenario from trp_check_ftele; -- 157
+select model, scenario from trp_check_psele; -- 280
+
+select model, scenario from trp_ele  -- 512, contain above four groups
+group by model, scenario 
+except
+select model, scenario from trp_check_rdps;
+
+
+
 
 
